048ebc1755aea7a7e6a1effd83c90bce3453a5c6
Glenn Terjesen <glenn.terjesen@gmail.com>
2026-03-31 14:55:55 +0200

    fix: update URLs and formula SHA after repo rename to homebrew-depbot-gen

--- Formula/depbot-gen.rb
+++ Formula/depbot-gen.rb
@@ -1,8 +1,8 @@
 class DepbotGen < Formula
   desc "Scan git repos and generate GitHub Dependabot configuration"
-  homepage "https://github.com/Glenn-Terjesen/depbot-gen"
-  url "https://github.com/Glenn-Terjesen/depbot-gen/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  homepage "https://github.com/Glenn-Terjesen/homebrew-depbot-gen"
+  url "https://github.com/Glenn-Terjesen/homebrew-depbot-gen/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "6a6e0cbe40b2ecaf189f082ceec57039bb7d2cfce0b28a3a76c22895f97a8c0e"
   license "MIT"
 
   depends_on "bash"
